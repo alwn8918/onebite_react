d2d8321c39d4ce016cee0b756073a906cbe0d9fa
김미주 <alwn8918@gmail.com>
2024-07-15 08:49:40 +0900

    section08-05

--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -1,15 +1,39 @@
+import { useState } from "react";
 import "./List.css";
 import TodoItem from "./TodoItem";
 
-const List = () => {
+const List = ({ todos }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+    // 문자열이 포함된 투두만 필터링
+    // 소문자/대문자 상관 없이 검색할 수 있도록 toLowerCase() 추가
+  };
+
+  const filteredTodos = getFilteredData();
+
   return (
     <div className="List">
       <h4>Todo List 🌱</h4>
-      <input placeholder="검색어를 입력하세요" />
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
       <div className="todos_wrapper">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {filteredTodos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} />;
+        })}
       </div>
     </div>
   );
