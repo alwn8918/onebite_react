84df1723a1548a04e7d13f0e986b9f2c74cfbb75
김미주 <alwn8918@gmail.com>
2024-07-14 09:01:56 +0900

    section08-03

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
 import Header from "./components/Header";
 import List from "./components/List";
-import TodoItem from "./components/TodoItem";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    data: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "토트넘 경기 보기",
+    data: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "티켓팅 하기",
+    data: new Date().getTime(),
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(mockData);
   return (
     <div className="App">
       <Header />
