d5e18c5d131238bbcf7398382174df245994bd65
MijuKim <alwn8918@gmail.com>
2024-07-25 03:39:52 +0900

    section10-04 useCallback과 함수 재생성 방지

--- section10/src/components/TodoItem.jsx
+++ section10/src/components/TodoItem.jsx
@@ -20,15 +20,17 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   );
 };
 
-export default memo(TodoItem, (prevProps, nextProps) => {
-  // 반환값에 따라 Props의 변화 판단
-  // T -> Props 바뀌지 않음 -> 리렌더링 X
-  // F -> Props 바뀜 -> 리렌더링 O
+// export default memo(TodoItem, (prevProps, nextProps) => {
+//   // 반환값에 따라 Props의 변화 판단
+//   // T -> Props 바뀌지 않음 -> 리렌더링 X
+//   // F -> Props 바뀜 -> 리렌더링 O
 
-  if (prevProps.id !== nextProps.id) return false;
-  if (prevProps.isDone !== nextProps.isDone) return false;
-  if (prevProps.content !== nextProps.content) return false;
-  if (prevProps.date !== nextProps.date) return false;
+//   if (prevProps.id !== nextProps.id) return false;
+//   if (prevProps.isDone !== nextProps.isDone) return false;
+//   if (prevProps.content !== nextProps.content) return false;
+//   if (prevProps.date !== nextProps.date) return false;
 
-  return true;
-});
+//   return true;
+// });
+
+export default memo(TodoItem);
