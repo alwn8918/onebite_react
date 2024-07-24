fa3362e7945cfdb1df0919b301be7890dec4bf86
MijuKim <alwn8918@gmail.com>
2024-07-25 03:56:17 +0900

    section11-01 Context

---
+++ section11/src/components/List.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import "./List.css";
+import TodoItem from "./TodoItem";
+import { useMemo } from "react";
+
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+    // 문자열이 포함된 투두만 필터링
+    // 소문자/대문자 상관 없이 검색할 수 있도록 toLowerCase() 추가
+  };
+
+  const filteredTodos = getFilteredData();
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+
+  // const { totalCount, doneCount, notDoneCount } = getAnalyzedData();
+
+  return (
+    <div className="List">
+      <h4>Todo List 🌱</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
